5ffee067ebb8cfaa866896f2b23e5984afa4e16d
Dusan Malusev <dusan.998@outlook.com>
2020-08-30 11:11:10 +0200

    Adding more CLI commands
    Signed-off-by: Dusan Malusev <dusan.998@outlook.com>

--- cli/main.go
+++ cli/main.go
@@ -8,29 +8,15 @@ import (
 
 	"github.com/BrosSquad/vaulguard/config"
 	"github.com/BrosSquad/vaulguard/db"
+	"github.com/BrosSquad/vaulguard/models"
 	"github.com/BrosSquad/vaulguard/services"
 	"github.com/spf13/cobra"
-	"gorm.io/gorm"
 )
 
 var (
 	applicationService services.ApplicationService
 	tokenService       services.TokenService
 )
-
-func connectToRelationalDatabaseAndMigrate(cfg *config.Config) *gorm.DB {
-	conn, err := db.ConnectToDatabaseProvider(cfg.Database, cfg.DatabaseDSN)
-
-	if err != nil {
-		log.Fatalf("Error while connection to PostgreSQL: %v", err)
-	}
-
-	if err := db.Migrate(cfg.StoreSecretInSql); err != nil {
-		log.Fatalf("Auto migration failed: %v", err)
-	}
-
-	return conn
-}
 
 var (
 	rootCmd *cobra.Command
@@ -39,6 +25,40 @@ var (
 func applicationCommands() *cobra.Command {
 	application := &cobra.Command{
 		Use: "app",
+	}	
+
+	list := &cobra.Command{
+		Use:  "list",
+		Long: "List all applications in the database",
+		Run: func(cmd *cobra.Command, args []string) {
+			iterate := func(apps []models.Application) error {
+				for _, app := range apps {
+					fmt.Printf("ID: %d, Name: %s\n", app.ID, app.Name)
+				}
+
+				return nil
+			}
+			err := applicationService.List(iterate)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+		},
+	}
+
+	findByName := &cobra.Command{
+		Use:  "by-name",
+		Long: "Search for application by it's name",
+		Args: cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			app, err := applicationService.GetByName(args[0])
+
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+
+			fmt.Printf("ID: %d, Name:%s\n", app.ID, app.Name)
+		},
 	}
 
 	create := &cobra.Command{
@@ -52,7 +72,14 @@ func applicationCommands() *cobra.Command {
 				log.Fatal(err.Error())
 			}
 
+			token := tokenService.Generate(app.ID)
+
+			if token == "" {
+				log.Fatal("Error while generating Auth Token")
+			}
+
 			fmt.Printf("New Application created: ID: %d Name: %s\n", app.ID, app.Name)
+			fmt.Printf("Auth Token: %s\n", token)
 		},
 	}
 	delete := &cobra.Command{
@@ -81,6 +108,8 @@ func applicationCommands() *cobra.Command {
 		},
 	}
 
+	application.AddCommand(list)
+	application.AddCommand(findByName)
 	application.AddCommand(create)
 	application.AddCommand(delete)
 
@@ -100,16 +129,24 @@ func parseCommands() error {
 }
 
 func main() {
-	cfg, err := config.NewConfig()
+	cfg, err := config.NewConfig(true)
 
 	if err != nil {
 		log.Fatalf("Error while creating app configuration: %v\n", err)
 	}
 
-	db := connectToRelationalDatabaseAndMigrate(&cfg)
+	conn, err := db.ConnectToDatabaseProvider(cfg.Database, cfg.DatabaseDSN)
 
-	applicationService = services.NewApplicationService(db)
-	tokenService = services.NewTokenService(db)
+	if err != nil {
+		log.Fatalf("Error while connection to PostgreSQL: %v", err)
+	}
+
+	if err := db.Migrate(cfg.StoreSecretInSql); err != nil {
+		log.Fatalf("Auto migration failed: %v", err)
+	}
+
+	applicationService = services.NewApplicationService(conn)
+	tokenService = services.NewTokenService(conn)
 
 	parseCommands()
 }
